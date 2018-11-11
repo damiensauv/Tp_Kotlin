de1f8ca620d1ac04fbc598980de917df6ab17aa0
damiensauv <damiensauvalle@gmail.com>
2018-11-11 16:53:29 +0100

    feat : add basic mousseListener git status

--- src/Gui/MainGui.kt
+++ src/Gui/MainGui.kt
@@ -3,9 +3,17 @@ package Gui
 import java.awt.Dimension
 import java.awt.EventQueue
 import java.awt.GridLayout
+import java.awt.event.MouseListener
 import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JPanel
+import java.awt.event.MouseAdapter
+import com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener
+import java.awt.event.MouseEvent
+import com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener
+import javax.swing.JLabel
+
+
 
 
 class MainGui(title: String) : JFrame() {
@@ -28,18 +36,27 @@ private fun createAndShowGUI() {
 
     val frame = MainGui("Simple")
 
+
     val buttonPanel = JPanel()
     val containerPanel = JPanel()
     buttonPanel.layout = GridLayout(10, 10)
 
     for (i in 0..99) {
-        buttonPanel.add(JButton(""))
+       val but = JButton(i.toString())
+        but.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                println("Clicked!")
+            }
+        })
+
+        buttonPanel.add(but)
     }
 
-    buttonPanel.preferredSize = Dimension(800, 800)
+
+    buttonPanel.preferredSize = Dimension(800, 800) 
     containerPanel.add(buttonPanel)
 
-    frame.contentPane.add(containerPanel)
+     frame.contentPane.add(containerPanel)
     frame.pack()
     frame.isVisible = true
 }
